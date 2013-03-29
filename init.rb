82d2ab546f2002d44a76534143066b98ed81cab0
Luke Gruber <luke.gru@gmail.com>
2013-03-29 12:29:00 -0400

    add mentel.com as author_url

--- init.rb
+++ init.rb
@@ -5,11 +5,11 @@ require_dependency File.expand_path(File.join(File.dirname(__FILE__), 'app/contr
 
 Redmine::Plugin.register :favorite_locations do
   name 'Favorite Locations plugin'
-  author 'Luke Gruber'
+  author 'Luke Gruber of Mentel'
   description 'Easily navigate to your most-accessed pages in Redmine'
   version '0.0.1'
-  url ''
-  author_url ''
+  url 'https://github.com/mentel/redmine-favorite-locations'
+  author_url 'http://www.mentel.com/'
 
   permission :favorite_locations, { :favorite_locations => [:index, :create, :update, :edit, :destroy] }, :public => true
 end
